2adfcff3b03fe2a03166693446b74cb779a9c14b
Sacylkowski <sacylkowski@Sarahs-MBP.austin.rr.com>
2022-04-05 16:36:14 -0500

    added health to player

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -4,7 +4,7 @@ const Potion = require("../lib/Potion");
 
 jest.mock("../lib/Potion");
 
-test("gets play'er stats as an objext", () => {
+test("gets player stats as an object", () => {
     const player = new Player("Dave");
 
     expect(player.getStats()).toHaveProperty("potions");
@@ -33,4 +33,33 @@ test("creates a player object", () => {
     expect(player.inventory).toEqual(
         expect.arrayContaining([expect.any(Object)])
     );
+});
+
+test("gets player's health value", () => {
+    const player = new Player("Dave");
+
+    expect(player.getHealth()).toEqual(expect.stringContaining(player.health.toString()));
+});
+
+test("checks if player is alive or not", () => {
+    const player = new Player("Dave");
+
+    expect(player.isAlive()).toBeTruthy();
+
+    player.health = 0;
+
+    expect(player.isAlive()).toBeFalsy();
+});
+
+test("subtracts from player's health", () => {
+    const player = new Player("Dave");
+    const oldHealth = player.health;
+
+    player.reduceHealth(5);
+
+    expect(player.health).toBe(oldHealth - 5);
+
+    player.reduceHealth(99999);
+
+    expect(player.health).toBe(0);
 });
