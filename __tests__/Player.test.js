e722282686a42091514cf576319245edfa381ffd
Sacylkowski <sacylkowski@Sarahs-MBP.austin.rr.com>
2022-04-05 15:42:41 -0500

    player objects working

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -1,7 +1,27 @@
+const { test, expect } = require("@jest/globals");
 const Player = require("../lib/Player");
 const Potion = require("../lib/Potion");
 
 jest.mock("../lib/Potion");
+
+test("gets play'er stats as an objext", () => {
+    const player = new Player("Dave");
+
+    expect(player.getStats()).toHaveProperty("potions");
+    expect(player.getStats()).toHaveProperty("health");
+    expect(player.getStats()).toHaveProperty("strength");
+    expect(player.getStats()).toHaveProperty("agility");
+});
+
+test("get inventory from player or returns false", () => {
+    const player = new Player("Dave");
+
+    expect(player.getInventory()).toEqual(expect.any(Array));
+
+    player.inventory = [];
+
+    expect(player.getInventory()).toEqual(false);
+});
 
 test("creates a player object", () => {
     const player = new Player("Dave");
